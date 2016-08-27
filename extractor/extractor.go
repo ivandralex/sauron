0a6576ea38095fc9e6a3f1742560ab29bde3c976
Andrew Ivanov <andrey.ivanov@onetwotrip.com>
2016-08-28 00:15:34 +0300

    Refactoring: added features package

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -6,14 +6,13 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"math"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
 	"github.com/paulbellamy/ratecounter"
+	"github.com/sauron/features"
 	"github.com/sauron/session"
 	"github.com/sauron/stat"
 )
@@ -49,65 +48,8 @@ var sessions = new(sstrg.SessionsTable)
 var emulatedTime time.Time
 var rpsCounter = ratecounter.NewRateCounter(10 * time.Second)
 
-//PathVector vector of features inherited from http path
-type PathVector struct {
-	//Delay of the first request (to this path) in the session
-	started float64
-	last    time.Time
-	//Total number of requests
-	counter int
-	//Delays between consecutive calls to this path
-	delays []float64
-	//Average delay
-	averageDelay float64
-	//Maximum delay
-	maxDelay float64
-	//Minimum delay
-	minDelay float64
-	//Delays after previous request to different path of the same content type
-	chainDelays []float64
-	//Has referrer been requested?
-	validRef bool
-}
-
-func (pv *PathVector) describe() []string {
-	vector := []string{
-		strconv.FormatInt(int64(pv.counter), 10),
-		strconv.FormatFloat(pv.started, 'f', 2, 64),
-		strconv.FormatFloat(pv.averageDelay, 'f', 2, 64),
-		strconv.FormatFloat(pv.minDelay, 'f', 2, 64),
-		strconv.FormatFloat(pv.maxDelay, 'f', 2, 64)}
-	//strconv.FormatBool(pv.validRef)}
-
-	return vector
-}
-
 func init() {
 	sessions.H = make(map[string]*sstrg.SessionHistory)
-}
-
-//FeatureVector feature representation of the session
-type FeatureVector struct {
-	//Vectors corresponding to every unique path
-	pathVectors        map[string]*PathVector
-	sessionDuration    float64
-	sessionStartHour   int
-	sessionStartMinute int
-	clientTimeZone     int
-}
-
-func (fv *FeatureVector) describe(pathsFilter []string) []string {
-	var finalVector []string
-	for _, path := range pathsFilter {
-		if pv, ok := fv.pathVectors[path]; ok {
-			finalVector = append(finalVector, pv.describe()...)
-		} else {
-			//Add NaN vector if path was not visited
-			finalVector = append(finalVector, "0", "0", "0", "0", "0")
-		}
-	}
-
-	return finalVector
 }
 
 //Start features extractor
@@ -123,13 +65,13 @@ func Start() {
 
 	//Dump features periodically
 	if config.beholdFeatures {
-		var topPaths = readTopPaths()
-		go startFeaturesBeholder(sessions, config.featuresPeriod, topPaths)
+		var targetPaths = readTargetPaths()
+		go startFeaturesBeholder(sessions, config.featuresPeriod, targetPaths)
 	}
 }
 
-func readTopPaths() []string {
-	f, err := os.Open("../configs/paths.csv")
+func readTargetPaths() []string {
+	f, err := os.Open("../configs/target_paths.csv")
 
 	if err != nil {
 		fmt.Println("Error readin top paths ", err)
@@ -203,19 +145,19 @@ func closeSessions() {
 	sessions.RUnlock()
 }
 
-func startFeaturesBeholder(sessions *sstrg.SessionsTable, periodSec int, topPaths []string) {
+func startFeaturesBeholder(sessions *sstrg.SessionsTable, periodSec int, targetPaths []string) {
 	// fire once per second
 	t := time.NewTicker(time.Second * time.Duration(periodSec))
 
 	w := csv.NewWriter(os.Stdout)
 
 	for {
-		dumpFeatures(w, sessions, topPaths)
+		dumpFeatures(w, sessions, targetPaths)
 		<-t.C
 	}
 }
 
-func dumpFeatures(w *csv.Writer, sessions *sstrg.SessionsTable, topPaths []string) {
+func dumpFeatures(w *csv.Writer, sessions *sstrg.SessionsTable, targetPaths []string) {
 	sessions.Lock()
 
 	for key, s := range sessions.H {
@@ -224,9 +166,7 @@ func dumpFeatures(w *csv.Writer, sessions *sstrg.SessionsTable, topPaths []strin
 			continue
 		}
 
-		var fv = extractFeatures(s)
-
-		var fvDesc = fv.describe(topPaths)
+		var fvDesc = pathvector.ExtractFeatures(s, targetPaths)
 
 		if err := w.Write(fvDesc); err != nil {
 			log.Fatalln("Error writing record to csv:", err)
@@ -320,59 +260,4 @@ func HandleRequest(sessionKey string, request *sstrg.RequestData) {
 	sessions.H[sessionKey].Requests = append(sessions.H[sessionKey].Requests, request)
 
 	sessions.Unlock()
-}
-
-func extractFeatures(s *sstrg.SessionHistory) *FeatureVector {
-	//sstrg.SortRequestsByTime(s.Requests)
-
-	var fv = new(FeatureVector)
-	//TODO: init it above
-	fv.pathVectors = make(map[string]*PathVector)
-
-	var validRef bool
-	//Build path vectors map from requests
-	for _, r := range s.Requests {
-		//fmt.Fprintf(os.Stdout, "%v\n", r.Time)
-
-		//Have referrer of this request been requested?
-		if _, ok := fv.pathVectors[r.Referer]; ok {
-			validRef = true
-		}
-
-		if _, pv := fv.pathVectors[r.Path]; !pv {
-			fv.pathVectors[r.Path] = new(PathVector)
-			fv.pathVectors[r.Path].started = r.Time.Sub(s.Started).Seconds()
-			fv.pathVectors[r.Path].minDelay = math.MaxFloat64
-			fv.pathVectors[r.Path].validRef = validRef
-		} else {
-			//Delay after the last request with the same path
-			var delay = r.Time.Sub(fv.pathVectors[r.Path].last).Seconds()
-			fv.pathVectors[r.Path].delays = append(fv.pathVectors[r.Path].delays, delay)
-			fv.pathVectors[r.Path].averageDelay += delay
-			//Update max delay
-			if delay > fv.pathVectors[r.Path].maxDelay {
-				fv.pathVectors[r.Path].maxDelay = delay
-			}
-			//Update min delay
-			if delay < fv.pathVectors[r.Path].minDelay {
-				fv.pathVectors[r.Path].minDelay = delay
-			}
-		}
-
-		//If validRef is false for a single request in a session it's gonna be false for corresponding PathVector
-		fv.pathVectors[r.Path].validRef = fv.pathVectors[r.Path].validRef && validRef
-		fv.pathVectors[r.Path].last = r.Time
-		fv.pathVectors[r.Path].counter++
-	}
-
-	for _, pathVector := range fv.pathVectors {
-		if pathVector.counter == 1 {
-			pathVector.minDelay = 0
-			pathVector.averageDelay = 0
-		} else {
-			pathVector.averageDelay /= float64(pathVector.counter - 1)
-		}
-	}
-
-	return fv
 }
---
+++ features/paths_vector.go
@@ -0,0 +1,129 @@
+package pathvector
+
+import (
+	"math"
+	"strconv"
+	"time"
+
+	"github.com/sauron/session"
+)
+
+//PathVector vector of features inherited from http path
+type PathVector struct {
+	//Delay of the first request (to this path) in the session
+	started float64
+	last    time.Time
+	//Total number of requests
+	counter int
+	//Delays between consecutive calls to this path
+	delays []float64
+	//Average delay
+	averageDelay float64
+	//Maximum delay
+	maxDelay float64
+	//Minimum delay
+	minDelay float64
+	//Delays after previous request to different path of the same content type
+	chainDelays []float64
+	//Has referrer been requested?
+	validRef bool
+}
+
+func (pv *PathVector) describe() []string {
+	vector := []string{
+		strconv.FormatInt(int64(pv.counter), 10),
+		strconv.FormatFloat(pv.started, 'f', 2, 64),
+		strconv.FormatFloat(pv.averageDelay, 'f', 2, 64),
+		strconv.FormatFloat(pv.minDelay, 'f', 2, 64),
+		strconv.FormatFloat(pv.maxDelay, 'f', 2, 64)}
+	//strconv.FormatBool(pv.validRef)}
+
+	return vector
+}
+
+//FeatureVector feature representation of the session
+type FeatureVector struct {
+	//Vectors corresponding to every unique path
+	pathVectors        map[string]*PathVector
+	sessionDuration    float64
+	sessionStartHour   int
+	sessionStartMinute int
+	clientTimeZone     int
+}
+
+func (fv *FeatureVector) describe(pathsFilter []string) []string {
+	var finalVector []string
+	for _, path := range pathsFilter {
+		if pv, ok := fv.pathVectors[path]; ok {
+			finalVector = append(finalVector, pv.describe()...)
+		} else {
+			//Add NaN vector if path was not visited
+			finalVector = append(finalVector, "0", "0", "0", "0", "0")
+		}
+	}
+
+	return finalVector
+}
+
+//ExtractFeatures extracts paths vector from session
+func ExtractFeatures(s *sstrg.SessionHistory, targetPaths []string) []string {
+	var fv = extractFeatureVector(s)
+
+	return fv.describe(targetPaths)
+}
+
+//Extracts paths vector from session
+func extractFeatureVector(s *sstrg.SessionHistory) *FeatureVector {
+	//sstrg.SortRequestsByTime(s.Requests)
+
+	var fv = new(FeatureVector)
+	//TODO: init it above
+	fv.pathVectors = make(map[string]*PathVector)
+
+	var validRef bool
+	//Build path vectors map from requests
+	for _, r := range s.Requests {
+		//fmt.Fprintf(os.Stdout, "%v\n", r.Time)
+
+		//Have referrer of this request been requested?
+		if _, ok := fv.pathVectors[r.Referer]; ok {
+			validRef = true
+		}
+
+		if _, pv := fv.pathVectors[r.Path]; !pv {
+			fv.pathVectors[r.Path] = new(PathVector)
+			fv.pathVectors[r.Path].started = r.Time.Sub(s.Started).Seconds()
+			fv.pathVectors[r.Path].minDelay = math.MaxFloat64
+			fv.pathVectors[r.Path].validRef = validRef
+		} else {
+			//Delay after the last request with the same path
+			var delay = r.Time.Sub(fv.pathVectors[r.Path].last).Seconds()
+			fv.pathVectors[r.Path].delays = append(fv.pathVectors[r.Path].delays, delay)
+			fv.pathVectors[r.Path].averageDelay += delay
+			//Update max delay
+			if delay > fv.pathVectors[r.Path].maxDelay {
+				fv.pathVectors[r.Path].maxDelay = delay
+			}
+			//Update min delay
+			if delay < fv.pathVectors[r.Path].minDelay {
+				fv.pathVectors[r.Path].minDelay = delay
+			}
+		}
+
+		//If validRef is false for a single request in a session it's gonna be false for corresponding PathVector
+		fv.pathVectors[r.Path].validRef = fv.pathVectors[r.Path].validRef && validRef
+		fv.pathVectors[r.Path].last = r.Time
+		fv.pathVectors[r.Path].counter++
+	}
+
+	for _, pathVector := range fv.pathVectors {
+		if pathVector.counter == 1 {
+			pathVector.minDelay = 0
+			pathVector.averageDelay = 0
+		} else {
+			pathVector.averageDelay /= float64(pathVector.counter - 1)
+		}
+	}
+
+	return fv
+}
