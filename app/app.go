e1ba7d56e59d0e287f2ba0b66a7c92d072ea30c5
Andrew Ivanov <andrey.ivanov@onetwotrip.com>
2017-03-03 22:17:40 +0300

    Small package refactoring

--- app/app.go
+++ app/app.go
@@ -1,10 +1,8 @@
 package sauron
 
 import (
-	"fmt"
 	"io"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -136,6 +134,7 @@ func startFeaturesBeholder(sessions *sstrg.SessionsTable, periodSec int) {
 		log.Fatalln("Could not open dump file for writing:", err)
 	}
 
+	//TODO: use composite serializable key
 	columnNames := []string{"ip", "user_agent"}
 	featureNames := defaultExtractor.GetFeaturesNames()
 	columnNames = append(columnNames, featureNames...)
@@ -149,23 +148,6 @@ func startFeaturesBeholder(sessions *sstrg.SessionsTable, periodSec int) {
 		dumpFeatures(w, sessions)
 		<-t.C
 	}
-}
-
-func printCSV(w io.Writer, row []string) error {
-	sep := ""
-	for _, cell := range row {
-		_, err := fmt.Fprintf(w, `%s"%s"`, sep, strings.Replace(cell, `"`, `""`, -1))
-		if err != nil {
-			return err
-		}
-		sep = ","
-	}
-	_, err := fmt.Fprintf(w, "\n")
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func dumpFeatures(w io.Writer, sessions *sstrg.SessionsTable) {
@@ -199,110 +181,4 @@ func dumpFeatures(w io.Writer, sessions *sstrg.SessionsTable) {
 	}
 
 	sessions.Unlock()
-}
-
-//StatHandler outputs current RPS
-func StatHandler(w http.ResponseWriter, r *http.Request) {
-	rps := rpsCounter.Rate() / 10
-
-	fmt.Fprintf(w, "RPS: %d", rps)
-}
-
-//RequestHandler handles incoming request
-func RequestHandler(w http.ResponseWriter, r *http.Request) {
-	//Form session key
-	var sessionKey = sstrg.GetSessionKey(r)
-	//Extract useful data from request
-	var request = sstrg.GetRequestData(r, config.useDataHeader)
-
-	HandleRequest(sessionKey, request)
-
-	fmt.Fprintf(w, "OK")
-}
-
-//HandleRequest handles parsed request
-func HandleRequest(sessionKey string, request *sstrg.RequestData) {
-	rpsCounter.Incr(1)
-	//Create stat struct for new session
-	sessions.Lock()
-
-	//Save new session to storage
-	if _, ok := sessions.H[sessionKey]; !ok {
-		sessions.H[sessionKey] = new(sstrg.SessionData)
-		sessions.H[sessionKey].Started = request.Time
-		sessions.H[sessionKey].IP = request.IP
-	}
-
-	//If session was inactive and was not deleted and we received request with same session key we re-activate session
-	sessions.H[sessionKey].Active = true
-	sessions.H[sessionKey].Ended = request.Time
-
-	//Update emulated value
-	if config.emulateTime {
-		emulatedTime = request.Time
-	}
-
-	//Save request to session
-	sessions.H[sessionKey].Requests = append(sessions.H[sessionKey].Requests, request)
-
-	sessions.Unlock()
-}
-
-//RawHandler outputs raw requests data for specified session key
-func RawHandler(w http.ResponseWriter, r *http.Request) {
-	var sessionKey = r.URL.Query().Get("key")
-
-	if _, ok := sessions.H[sessionKey]; !ok {
-		fmt.Fprint(w, "Not Found")
-		return
-	}
-
-	sessions.RLock()
-
-	var session = sessions.H[sessionKey]
-
-	fmt.Fprintf(w, "Started: %v\nLast: %v\nRequests: %d\nActive: %v\n\n", session.Started, session.Ended, len(session.Requests), session.Active)
-
-	for _, r := range sessions.H[sessionKey].Requests {
-
-		fmt.Fprintf(w, "%s %s\n", r.Method, r.Path)
-
-		for _, c := range r.Cookies {
-			fmt.Fprintf(w, "%s=%s;", c.Name, c.Path)
-		}
-
-		fmt.Fprintf(w, "\n")
-
-		for k, h := range r.Header {
-			fmt.Fprintf(w, "%s: %s\n", k, h[0])
-		}
-
-		fmt.Fprintf(w, "\n\n\n\n")
-	}
-
-	sessions.RUnlock()
-}
-
-//SessionCheckHandler checks session with default detector
-func SessionCheckHandler(w http.ResponseWriter, r *http.Request) {
-	var sessionKey = r.URL.Query().Get("key")
-
-	if sessionKey == "" {
-		sessionKey = sstrg.GetSessionKey(r)
-	}
-
-	sessions.RLock()
-
-	if _, ok := sessions.H[sessionKey]; !ok {
-		fmt.Fprint(w, "Key Not Found: "+sessionKey)
-		sessions.RUnlock()
-		return
-	}
-
-	var session = sessions.H[sessionKey]
-	var label = defaultDetector.GetLabel(session)
-
-	sessions.RUnlock()
-
-	fmt.Fprint(w, label)
 }
---
+++ app/http_handlers.go
@@ -0,0 +1,114 @@
+package sauron
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sauron/session"
+)
+
+//StatHandler outputs current RPS
+func StatHandler(w http.ResponseWriter, r *http.Request) {
+	rps := rpsCounter.Rate() / 10
+
+	fmt.Fprintf(w, "RPS: %d", rps)
+}
+
+//RequestHandler handles incoming request
+func RequestHandler(w http.ResponseWriter, r *http.Request) {
+	//Form session key
+	var sessionKey = sstrg.GetSessionKey(r)
+	//Extract useful data from request
+	var request = sstrg.GetRequestData(r, config.useDataHeader)
+
+	HandleRequest(sessionKey, request)
+
+	fmt.Fprintf(w, "OK")
+}
+
+//HandleRequest handles parsed request
+func HandleRequest(sessionKey string, request *sstrg.RequestData) {
+	rpsCounter.Incr(1)
+	//Create stat struct for new session
+	sessions.Lock()
+
+	//Save new session to storage
+	if _, ok := sessions.H[sessionKey]; !ok {
+		sessions.H[sessionKey] = new(sstrg.SessionData)
+		sessions.H[sessionKey].Started = request.Time
+		sessions.H[sessionKey].IP = request.IP
+	}
+
+	//If session was inactive and was not deleted and we received request with same session key we re-activate session
+	sessions.H[sessionKey].Active = true
+	sessions.H[sessionKey].Ended = request.Time
+
+	//Update emulated value
+	if config.emulateTime {
+		emulatedTime = request.Time
+	}
+
+	//Save request to session
+	sessions.H[sessionKey].Requests = append(sessions.H[sessionKey].Requests, request)
+
+	sessions.Unlock()
+}
+
+//RawHandler outputs raw requests data for specified session key
+func RawHandler(w http.ResponseWriter, r *http.Request) {
+	var sessionKey = r.URL.Query().Get("key")
+
+	if _, ok := sessions.H[sessionKey]; !ok {
+		fmt.Fprint(w, "Not Found")
+		return
+	}
+
+	sessions.RLock()
+
+	var session = sessions.H[sessionKey]
+
+	fmt.Fprintf(w, "Started: %v\nLast: %v\nRequests: %d\nActive: %v\n\n", session.Started, session.Ended, len(session.Requests), session.Active)
+
+	for _, r := range sessions.H[sessionKey].Requests {
+
+		fmt.Fprintf(w, "%s %s\n", r.Method, r.Path)
+
+		for _, c := range r.Cookies {
+			fmt.Fprintf(w, "%s=%s;", c.Name, c.Path)
+		}
+
+		fmt.Fprintf(w, "\n")
+
+		for k, h := range r.Header {
+			fmt.Fprintf(w, "%s: %s\n", k, h[0])
+		}
+
+		fmt.Fprintf(w, "\n\n\n\n")
+	}
+
+	sessions.RUnlock()
+}
+
+//SessionCheckHandler checks session with default detector
+func SessionCheckHandler(w http.ResponseWriter, r *http.Request) {
+	var sessionKey = r.URL.Query().Get("key")
+
+	if sessionKey == "" {
+		sessionKey = sstrg.GetSessionKey(r)
+	}
+
+	sessions.RLock()
+
+	if _, ok := sessions.H[sessionKey]; !ok {
+		fmt.Fprint(w, "Key Not Found: "+sessionKey)
+		sessions.RUnlock()
+		return
+	}
+
+	var session = sessions.H[sessionKey]
+	var label = defaultDetector.GetLabel(session)
+
+	sessions.RUnlock()
+
+	fmt.Fprint(w, label)
+}
